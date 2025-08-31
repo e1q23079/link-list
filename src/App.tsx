b2ce5bfc031e1ce80985dbbf821ebb9d20b944dd
e1q23079 <e1q23079@oit.ac.jp>
2025-08-31 14:01:00 +0900

    リンクリスト表示

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 // import './App.css'
-import { container } from './style.css.ts'
+import logo from './assets/icon.png'
+import { container2, discription, linkDiscription, linkItem, linkTitle, linkUrl, title, userName } from './style.css.ts'
 
 function App() {
   return (
     <>
-      <div className={container}>
-        Hello World
+      <div className={title}>
+        <img src={logo} alt="logo" />
+        <div className={userName}>e1q23079</div>
+        <div className={discription}>日々挑戦し続ける，情報系学生の記録</div>
+      </div>
+
+      <div className={container2}>
+        <div className={linkItem}>
+          <div className={linkTitle}>| e1q23079's portfolio</div>
+          <div className={linkDiscription}>
+            これまでに制作した作品やプロジェクトを紹介しているポートフォリオサイトです．
+          </div>
+          <a href="https://e1q23079.github.io/portfolio" className={linkUrl}>e1q23079's portfolio</a>
+        </div>
       </div>
     </>
   )
--- src/style.css.ts
+++ src/style.css.ts
@@ -1,5 +1,71 @@
 import { style } from "@vanilla-extract/css";
-
-export const container = style({
-    color: "red"
+// タイトル
+export const title = style({
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    color: "#0E2841",
+    padding: "10px",
 });
+
+// ユーザーネーム
+export const userName = style({
+    fontSize: "x-large",
+    fontWeight: "bold"
+});
+
+// 説明文
+export const discription = style({
+    fontSize: "medium",
+});
+
+// リンクリストコンテナ
+export const container = style({
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#8ED973",
+});
+
+// リンクアイテム
+export const linkItem = style({
+    margin: "10px",
+    padding: "10px",
+    backgroundColor: "white",
+    borderRadius: "10px",
+    border: "solid #8ED973",
+    color: "#0E2841",
+});
+
+// リンクタイトル
+export const linkTitle = style({
+    fontSize: "large",
+    fontWeight: "bold",
+    paddingBottom: "5px",
+    padding: "5px",
+    borderBottom: "solid #0E2841",
+});
+
+// リンク説明文
+export const linkDiscription = style({
+    padding: "5px"
+});
+
+// リンクURL
+export const linkUrl = style({
+    display: "flex",
+    padding: "5px",
+    width: "fit-content",
+    marginLeft: "auto",
+    ':hover': {
+        backgroundColor: "#D3F9B1",
+    }
+});
+
+
+export const container2 = style({
+    flexDirection: "column",
+    padding: "10px"
+});
+
