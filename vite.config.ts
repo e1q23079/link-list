41a1b56a05b2b57447eff237194cbb3f7b0c433a
e1q23079 <e1q23079@oit.ac.jp>
2025-08-31 16:10:23 +0900

    バグ修正

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), vanillaExtractPlugin()],
-  base: '/./'
+  base: '/link-list/'
 })
